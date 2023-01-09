ef3b766e9bbe34bd4a6ce5c8554dbc6d048300b4
AvanuTv <98516127+AvanuTv@users.noreply.github.com>
2023-01-09 12:55:26 +0100

    Velocity Tracking

--- Assets/Scripts/ControllerVelocity.cs
+++ Assets/Scripts/ControllerVelocity.cs
@@ -6,8 +6,18 @@ using UnityEngine.InputSystem;
 public class ControllerVelocity : MonoBehaviour
 {
     public InputActionProperty velocityProperty;
+    public string controllerHand = "none";
+    public OVRInput.Controller controller;
 
     public Vector3 Velocity { get; private set; } = Vector3.zero;
+
+    private void Start()
+    {
+        if (controllerHand.Equals("none")) controller = OVRInput.Controller.None;
+        else if (controllerHand.Equals("leftTouch")) controller = OVRInput.Controller.LTouch;
+        else if (controllerHand.Equals("rightTouch")) controller = OVRInput.Controller.RTouch;
+
+    }
 
     private void Update()
     {
--- Assets/Scripts/VelocityInteractable.cs
+++ Assets/Scripts/VelocityInteractable.cs
@@ -4,24 +4,28 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class VelocityInteractable : XRGrabInteractable
 {
     private ControllerVelocity controllerVelocity = null;
+    public OVRInput.Controller controller;
     private MeshRenderer meshRenderer = null;
 
     protected override void Awake()
     {
         base.Awake();
-        meshRenderer = GetComponent<MeshRenderer>();    
+        meshRenderer = GetComponent<MeshRenderer>();
+        controller = OVRInput.Controller.None;
     }
 
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         base.OnSelectEntered(args);
         controllerVelocity = args.interactorObject.transform.GetComponent<ControllerVelocity>();
+        controller = controllerVelocity.controller;
     }
 
     protected override void OnSelectExited(SelectExitEventArgs args)
     {
         base.OnSelectExited(args);
         controllerVelocity = null;
+        controller = OVRInput.Controller.None;
     }
 
     public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
@@ -31,14 +35,25 @@ public class VelocityInteractable : XRGrabInteractable
         if (isSelected)
         {
             UpdateColorBasedOnVelocity();
+            //print($"{controllerVelocity} | {controllerVelocity.Velocity}");
         }
     }
 
     private void UpdateColorBasedOnVelocity()
     {
-        print($"{controllerVelocity} | {controllerVelocity.Velocity}");
         Vector3 velocity = controllerVelocity ? controllerVelocity.Velocity : Vector3.zero;
         Color color = new(velocity.x, velocity.y, velocity.z);
         meshRenderer.material.color = color;
+    }
+
+    public GameObject RightHandAnchor;
+    private Vector3 lastPosition;
+
+    private void FixedUpdate()
+    {
+        var velocity = OVRInput.GetLocalControllerAngularVelocity(controller);
+        print($"Velocity {velocity}");
+        // Debug.Log($"Speed: {string.Format("0:0.00", speed)}");
+
     }
 }
